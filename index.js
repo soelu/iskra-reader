40453f7b99f0de6437103548c9e51499f3ffa9a1
Sören Lubitz <soeren.lubitz@gmail.com>
2017-01-14 20:59:02 +0100

    ready for first e2e device test

--- index.js
+++ index.js
@@ -2,21 +2,32 @@
 var serialport = require('serialport');
 // var SerialPort = serialport.SerialPort;
 var parsers = serialport.parsers;
+const fs = require('fs');
 
-var port = new serialport('/dev/tty.usbserial-141', {
-  baudRate: 300,
-  dataBits: 7,
-  stopBits: 1,
-  parity: 'even',
-  parser: parsers.readline('\r\n')
+
+var port = new serialport('/dev/ttyUSB0', {
+	baudRate: 300,
+	dataBits: 7,
+	stopBits: 1,
+	parity: 'even',
+	parser: parsers.readline('\r\n')
 });
 
 port.on('open', function() {
-  console.log('Port open');
-  port.write('/?!\n');
+	console.log('Port open');
+	port.write('/?!\r\n');
 });
 
 port.on('data', function(data) {
-  console.log(data);
+	console.log('Line from file:', data);
+  var d = data;
+	if (d.lastIndexOf('/ISk5MT174', 0) === 0) {
+		d = '\n' + new Date().getTime() + ';' + d;
+	} else {
+		d = ';' + d;
+	}
+	fs.appendFile('data.csv', d, function (err) {
+	  	if (err) return console.log(err);
+	});
 });
 
